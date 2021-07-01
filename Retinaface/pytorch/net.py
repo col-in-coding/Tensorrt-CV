3dc1a932ef42dc29a50598dad803335dadf0b0ba
lihaoxiang1989 <hxl.colin@gmail.com>
2021-07-01 17:58:56 +0800

    format files

--- Retinaface/pytorch/net.py
+++ Retinaface/pytorch/net.py
@@ -8,11 +8,9 @@ from torch.autograd import Variable
 
 
 def conv_bn(inp, oup, stride=1, leaky=0):
-    return nn.Sequential(
-        nn.Conv2d(inp, oup, 3, stride, 1, bias=False),
-        nn.BatchNorm2d(oup),
-        nn.LeakyReLU(negative_slope=leaky, inplace=True)
-    )
+    return nn.Sequential(nn.Conv2d(inp, oup, 3, stride, 1, bias=False),
+                         nn.BatchNorm2d(oup),
+                         nn.LeakyReLU(negative_slope=leaky, inplace=True))
 
 
 def conv_bn_no_relu(inp, oup, stride):
@@ -23,11 +21,9 @@ def conv_bn_no_relu(inp, oup, stride):
 
 
 def conv_bn1X1(inp, oup, stride, leaky=0):
-    return nn.Sequential(
-        nn.Conv2d(inp, oup, 1, stride, padding=0, bias=False),
-        nn.BatchNorm2d(oup),
-        nn.LeakyReLU(negative_slope=leaky, inplace=True)
-    )
+    return nn.Sequential(nn.Conv2d(inp, oup, 1, stride, padding=0, bias=False),
+                         nn.BatchNorm2d(oup),
+                         nn.LeakyReLU(negative_slope=leaky, inplace=True))
 
 
 def conv_dw(inp, oup, stride, leaky=0.1):
@@ -35,7 +31,6 @@ def conv_dw(inp, oup, stride, leaky=0.1):
         nn.Conv2d(inp, inp, 3, stride, 1, groups=inp, bias=False),
         nn.BatchNorm2d(inp),
         nn.LeakyReLU(negative_slope=leaky, inplace=True),
-
         nn.Conv2d(inp, oup, 1, 1, 0, bias=False),
         nn.BatchNorm2d(oup),
         nn.LeakyReLU(negative_slope=leaky, inplace=True),
@@ -49,17 +44,23 @@ class SSH(nn.Module):
         leaky = 0
         if (out_channel <= 64):
             leaky = 0.1
-        self.conv3X3 = conv_bn_no_relu(in_channel, out_channel//2, stride=1)
+        self.conv3X3 = conv_bn_no_relu(in_channel, out_channel // 2, stride=1)
 
-        self.conv5X5_1 = conv_bn(
-            in_channel, out_channel//4, stride=1, leaky=leaky)
-        self.conv5X5_2 = conv_bn_no_relu(
-            out_channel//4, out_channel//4, stride=1)
+        self.conv5X5_1 = conv_bn(in_channel,
+                                 out_channel // 4,
+                                 stride=1,
+                                 leaky=leaky)
+        self.conv5X5_2 = conv_bn_no_relu(out_channel // 4,
+                                         out_channel // 4,
+                                         stride=1)
 
-        self.conv7X7_2 = conv_bn(
-            out_channel//4, out_channel//4, stride=1, leaky=leaky)
-        self.conv7x7_3 = conv_bn_no_relu(
-            out_channel//4, out_channel//4, stride=1)
+        self.conv7X7_2 = conv_bn(out_channel // 4,
+                                 out_channel // 4,
+                                 stride=1,
+                                 leaky=leaky)
+        self.conv7x7_3 = conv_bn_no_relu(out_channel // 4,
+                                         out_channel // 4,
+                                         stride=1)
 
     def forward(self, input):
         conv3X3 = self.conv3X3(input)
@@ -81,12 +82,18 @@ class FPN(nn.Module):
         leaky = 0
         if (out_channels <= 64):
             leaky = 0.1
-        self.output1 = conv_bn1X1(
-            in_channels_list[0], out_channels, stride=1, leaky=leaky)
-        self.output2 = conv_bn1X1(
-            in_channels_list[1], out_channels, stride=1, leaky=leaky)
-        self.output3 = conv_bn1X1(
-            in_channels_list[2], out_channels, stride=1, leaky=leaky)
+        self.output1 = conv_bn1X1(in_channels_list[0],
+                                  out_channels,
+                                  stride=1,
+                                  leaky=leaky)
+        self.output2 = conv_bn1X1(in_channels_list[1],
+                                  out_channels,
+                                  stride=1,
+                                  leaky=leaky)
+        self.output3 = conv_bn1X1(in_channels_list[2],
+                                  out_channels,
+                                  stride=1,
+                                  leaky=leaky)
 
         self.merge1 = conv_bn(out_channels, out_channels, leaky=leaky)
         self.merge2 = conv_bn(out_channels, out_channels, leaky=leaky)
@@ -99,13 +106,17 @@ class FPN(nn.Module):
         output2 = self.output2(input[1])
         output3 = self.output3(input[2])
 
-        up3 = F.interpolate(output3, size=[output2.size(
-            2), output2.size(3)], mode="nearest")
+        up3 = F.interpolate(output3,
+                            size=[output2.size(2),
+                                  output2.size(3)],
+                            mode="nearest")
         output2 = output2 + up3
         output2 = self.merge2(output2)
 
-        up2 = F.interpolate(output2, size=[output1.size(
-            2), output1.size(3)], mode="nearest")
+        up2 = F.interpolate(output2,
+                            size=[output1.size(2),
+                                  output1.size(3)],
+                            mode="nearest")
         output1 = output1 + up2
         output1 = self.merge1(output1)
 
@@ -117,8 +128,8 @@ class MobileNetV1(nn.Module):
     def __init__(self):
         super(MobileNetV1, self).__init__()
         self.stage1 = nn.Sequential(
-            conv_bn(3, 8, 2, leaky=0.1),    # 3
-            conv_dw(8, 16, 1),   # 7
+            conv_bn(3, 8, 2, leaky=0.1),  # 3
+            conv_dw(8, 16, 1),  # 7
             conv_dw(16, 32, 2),  # 11
             conv_dw(32, 32, 1),  # 19
             conv_dw(32, 64, 2),  # 27
--- Retinaface/pytorch/retinaface.py
+++ Retinaface/pytorch/retinaface.py
@@ -1,63 +1,82 @@
 import torch
+import numpy as np
 import torch.nn as nn
-import torchvision.models.detection.backbone_utils as backbone_utils
+import torchvision
 import torchvision.models._utils as _utils
 import torch.nn.functional as F
-from collections import OrderedDict
 
-from .net import MobileNetV1 as MobileNetV1
-from .net import FPN as FPN
-from .net import SSH as SSH
+from .prior_box import PriorBox
+from .net import (MobileNetV1, FPN, SSH)
+from .config import cfg_re50
 
+NMS_THRESHOLD = 0.4
+KEEP_TOP_K = 100
 
 
 class ClassHead(nn.Module):
-    def __init__(self,inchannels=512,num_anchors=3):
-        super(ClassHead,self).__init__()
+    def __init__(self, inchannels=512, num_anchors=3):
+        super(ClassHead, self).__init__()
         self.num_anchors = num_anchors
-        self.conv1x1 = nn.Conv2d(inchannels,self.num_anchors*2,kernel_size=(1,1),stride=1,padding=0)
+        self.conv1x1 = nn.Conv2d(inchannels,
+                                 self.num_anchors * 2,
+                                 kernel_size=(1, 1),
+                                 stride=1,
+                                 padding=0)
 
-    def forward(self,x):
+    def forward(self, x):
         out = self.conv1x1(x)
-        out = out.permute(0,2,3,1).contiguous()
-        
+        out = out.permute(0, 2, 3, 1).contiguous()
+
         return out.view(out.shape[0], -1, 2)
 
-class BboxHead(nn.Module):
-    def __init__(self,inchannels=512,num_anchors=3):
-        super(BboxHead,self).__init__()
-        self.conv1x1 = nn.Conv2d(inchannels,num_anchors*4,kernel_size=(1,1),stride=1,padding=0)
 
-    def forward(self,x):
+class BboxHead(nn.Module):
+    def __init__(self, inchannels=512, num_anchors=3):
+        super(BboxHead, self).__init__()
+        self.conv1x1 = nn.Conv2d(inchannels,
+                                 num_anchors * 4,
+                                 kernel_size=(1, 1),
+                                 stride=1,
+                                 padding=0)
+
+    def forward(self, x):
         out = self.conv1x1(x)
-        out = out.permute(0,2,3,1).contiguous()
+        out = out.permute(0, 2, 3, 1).contiguous()
 
         return out.view(out.shape[0], -1, 4)
 
-class LandmarkHead(nn.Module):
-    def __init__(self,inchannels=512,num_anchors=3):
-        super(LandmarkHead,self).__init__()
-        self.conv1x1 = nn.Conv2d(inchannels,num_anchors*10,kernel_size=(1,1),stride=1,padding=0)
 
-    def forward(self,x):
+class LandmarkHead(nn.Module):
+    def __init__(self, inchannels=512, num_anchors=3):
+        super(LandmarkHead, self).__init__()
+        self.conv1x1 = nn.Conv2d(inchannels,
+                                 num_anchors * 10,
+                                 kernel_size=(1, 1),
+                                 stride=1,
+                                 padding=0)
+
+    def forward(self, x):
         out = self.conv1x1(x)
-        out = out.permute(0,2,3,1).contiguous()
+        out = out.permute(0, 2, 3, 1).contiguous()
 
         return out.view(out.shape[0], -1, 10)
 
+
 class RetinaFace(nn.Module):
-    def __init__(self, cfg = None, phase = 'train'):
+    def __init__(self, cfg=None, phase='train'):
         """
         :param cfg:  Network related settings.
         :param phase: train or test.
         """
-        super(RetinaFace,self).__init__()
+        super(RetinaFace, self).__init__()
         self.phase = phase
         backbone = None
         if cfg['name'] == 'mobilenet0.25':
             backbone = MobileNetV1()
             if cfg['pretrain']:
-                checkpoint = torch.load("./weights/mobilenetV1X0.25_pretrain.tar", map_location=torch.device('cpu'))
+                checkpoint = torch.load(
+                    "./weights/mobilenetV1X0.25_pretrain.tar",
+                    map_location=torch.device('cpu'))
                 from collections import OrderedDict
                 new_state_dict = OrderedDict()
                 for k, v in checkpoint['state_dict'].items():
@@ -69,7 +88,8 @@ class RetinaFace(nn.Module):
             import torchvision.models as models
             backbone = models.resnet50(pretrained=cfg['pretrain'])
 
-        self.body = _utils.IntermediateLayerGetter(backbone, cfg['return_layers'])
+        self.body = _utils.IntermediateLayerGetter(backbone,
+                                                   cfg['return_layers'])
         in_channels_stage2 = cfg['in_channel']
         in_channels_list = [
             in_channels_stage2 * 2,
@@ -77,34 +97,37 @@ class RetinaFace(nn.Module):
             in_channels_stage2 * 8,
         ]
         out_channels = cfg['out_channel']
-        self.fpn = FPN(in_channels_list,out_channels)
+        self.fpn = FPN(in_channels_list, out_channels)
         self.ssh1 = SSH(out_channels, out_channels)
         self.ssh2 = SSH(out_channels, out_channels)
         self.ssh3 = SSH(out_channels, out_channels)
 
-        self.ClassHead = self._make_class_head(fpn_num=3, inchannels=cfg['out_channel'])
-        self.BboxHead = self._make_bbox_head(fpn_num=3, inchannels=cfg['out_channel'])
-        self.LandmarkHead = self._make_landmark_head(fpn_num=3, inchannels=cfg['out_channel'])
+        self.ClassHead = self._make_class_head(fpn_num=3,
+                                               inchannels=cfg['out_channel'])
+        self.BboxHead = self._make_bbox_head(fpn_num=3,
+                                             inchannels=cfg['out_channel'])
+        self.LandmarkHead = self._make_landmark_head(
+            fpn_num=3, inchannels=cfg['out_channel'])
 
-    def _make_class_head(self,fpn_num=3,inchannels=64,anchor_num=2):
+    def _make_class_head(self, fpn_num=3, inchannels=64, anchor_num=2):
         classhead = nn.ModuleList()
         for i in range(fpn_num):
-            classhead.append(ClassHead(inchannels,anchor_num))
+            classhead.append(ClassHead(inchannels, anchor_num))
         return classhead
-    
-    def _make_bbox_head(self,fpn_num=3,inchannels=64,anchor_num=2):
+
+    def _make_bbox_head(self, fpn_num=3, inchannels=64, anchor_num=2):
         bboxhead = nn.ModuleList()
         for i in range(fpn_num):
-            bboxhead.append(BboxHead(inchannels,anchor_num))
+            bboxhead.append(BboxHead(inchannels, anchor_num))
         return bboxhead
 
-    def _make_landmark_head(self,fpn_num=3,inchannels=64,anchor_num=2):
+    def _make_landmark_head(self, fpn_num=3, inchannels=64, anchor_num=2):
         landmarkhead = nn.ModuleList()
         for i in range(fpn_num):
-            landmarkhead.append(LandmarkHead(inchannels,anchor_num))
+            landmarkhead.append(LandmarkHead(inchannels, anchor_num))
         return landmarkhead
 
-    def forward(self,inputs):
+    def forward(self, inputs):
         out = self.body(inputs)
 
         # FPN
@@ -116,12 +139,152 @@ class RetinaFace(nn.Module):
         feature3 = self.ssh3(fpn[2])
         features = [feature1, feature2, feature3]
 
-        bbox_regressions = torch.cat([self.BboxHead[i](feature) for i, feature in enumerate(features)], dim=1)
-        classifications = torch.cat([self.ClassHead[i](feature) for i, feature in enumerate(features)],dim=1)
-        ldm_regressions = torch.cat([self.LandmarkHead[i](feature) for i, feature in enumerate(features)], dim=1)
+        bbox_regressions = torch.cat(
+            [self.BboxHead[i](feature) for i, feature in enumerate(features)],
+            dim=1)
+        classifications = torch.cat(
+            [self.ClassHead[i](feature) for i, feature in enumerate(features)],
+            dim=1)
+        ldm_regressions = torch.cat([
+            self.LandmarkHead[i](feature) for i, feature in enumerate(features)
+        ],
+                                    dim=1)
 
         if self.phase == 'train':
             output = (bbox_regressions, classifications, ldm_regressions)
         else:
-            output = (bbox_regressions, F.softmax(classifications, dim=-1), ldm_regressions)
+            output = (bbox_regressions, F.softmax(classifications,
+                                                  dim=-1), ldm_regressions)
         return output
+
+
+def decode(loc, priors, variances):
+    """Decode locations from predictions using priors to undo
+    the encoding we did for offset regression at train time.
+    Args:
+        loc (tensor): location predictions for loc layers,
+            Shape: [num_priors,4]
+        priors (tensor): Prior boxes in center-offset form.
+            Shape: [num_priors,4].
+        variances: (list[float]) Variances of priorboxes
+    Return:
+        decoded bounding box predictions
+    """
+
+    boxes = torch.cat(
+        (priors[:, :2] + loc[:, :2] * variances[0] * priors[:, 2:],
+         priors[:, 2:] * torch.exp(loc[:, 2:] * variances[1])), 1)
+    boxes[:, :2] -= boxes[:, 2:] / 2
+    boxes[:, 2:] += boxes[:, :2]
+    return boxes
+
+
+def decode_landm(pre, priors, variances):
+    """Decode landm from predictions using priors to undo
+    the encoding we did for offset regression at train time.
+    Args:
+        pre (tensor): landm predictions for loc layers,
+            Shape: [num_priors,10]
+        priors (tensor): Prior boxes in center-offset form.
+            Shape: [num_priors,4].
+        variances: (list[float]) Variances of priorboxes
+    Return:
+        decoded landm predictions
+    """
+    landms = torch.cat((
+        priors[:, :2] + pre[:, :2] * variances[0] * priors[:, 2:],
+        priors[:, :2] + pre[:, 2:4] * variances[0] * priors[:, 2:],
+        priors[:, :2] + pre[:, 4:6] * variances[0] * priors[:, 2:],
+        priors[:, :2] + pre[:, 6:8] * variances[0] * priors[:, 2:],
+        priors[:, :2] + pre[:, 8:10] * variances[0] * priors[:, 2:],
+    ),
+                       dim=1)
+    return landms
+
+
+def load_model(model, pretrained_path, load_to_cpu):
+    print('Loading pretrained model from {}'.format(pretrained_path))
+    if load_to_cpu:
+        pretrained_dict = torch.load(pretrained_path,
+                                     map_location=lambda storage, loc: storage)
+    else:
+        device = torch.cuda.current_device()
+        pretrained_dict = torch.load(
+            pretrained_path,
+            map_location=lambda storage, loc: storage.cuda(device))
+    if "state_dict" in pretrained_dict.keys():
+        pretrained_dict = remove_prefix(pretrained_dict['state_dict'],
+                                        'module.')
+    else:
+        pretrained_dict = remove_prefix(pretrained_dict, 'module.')
+    check_keys(model, pretrained_dict)
+    model.load_state_dict(pretrained_dict, strict=False)
+    return model
+
+
+def remove_prefix(state_dict, prefix):
+    ''' Old style model is stored with all names of parameters sharing common prefix 'module.' '''
+    print('remove prefix \'{}\''.format(prefix))
+    f = lambda x: x.split(prefix, 1)[-1] if x.startswith(prefix) else x
+    return {f(key): value for key, value in state_dict.items()}
+
+
+def check_keys(model, pretrained_state_dict):
+    ckpt_keys = set(pretrained_state_dict.keys())
+    model_keys = set(model.state_dict().keys())
+    used_pretrained_keys = model_keys & ckpt_keys
+    unused_pretrained_keys = ckpt_keys - model_keys
+    missing_keys = model_keys - ckpt_keys
+    print('Missing keys:{}'.format(len(missing_keys)))
+    print('Unused checkpoint keys:{}'.format(len(unused_pretrained_keys)))
+    print('Used keys:{}'.format(len(used_pretrained_keys)))
+    assert len(
+        used_pretrained_keys) > 0, 'load NONE from pretrained checkpoint'
+    return True
+
+
+def postprocess(outputs, confidence_threshold=0.7, cfg=cfg_re50, device='cpu'):
+    loc = outputs[0].reshape((43008, 4))
+    conf = outputs[1].reshape((43008, 2))
+    landms = outputs[2].reshape((43008, 10))
+
+    priorbox = PriorBox(cfg_re50, (1024, 1024))
+    priors = priorbox.forward()
+
+    # decode boxes
+    boxes = decode(loc, priors.data, cfg_re50['variance'])
+    scale = torch.Tensor([1024, 1024, 1024, 1024])
+    scale = scale.to(device)
+    boxes = boxes * scale
+
+    scores = conf[:, 1]
+
+    landms = decode_landm(landms, priors.data, cfg_re50['variance'])
+    scale1 = np.array([1024] * 10)
+    # scale1 = torch.from_numpy(scale1).to(device)
+    landms = landms * scale1
+
+    # ignore low scores
+    inds = torch.where(scores > confidence_threshold)[0]
+    boxes = boxes[inds]
+    landms = landms[inds]
+    scores = scores[inds]
+
+    # keep top-K before NMS
+    top_k = 5000
+    order = scores.topk(min(top_k, len(scores))).indices
+    boxes = boxes[order]
+    landms = landms[order]
+    scores = scores[order]
+
+    # do NMS
+    keep = torchvision.ops.nms(boxes, scores, NMS_THRESHOLD)
+    landms = landms[keep]
+    boxes = boxes[keep]
+    scores = scores[keep]
+    dets = torch.hstack((boxes, scores.reshape(-1, 1)))
+
+    # keep top-K faster NMS
+    dets = dets[:KEEP_TOP_K, :]
+    landms = landms[:KEEP_TOP_K, :]
+    return dets.cpu().numpy(), landms.cpu().numpy()
