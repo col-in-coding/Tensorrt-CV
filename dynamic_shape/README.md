e2578f0ec316858fc942ad90b30d919d243342cb
col-in-coding <hxl.colin@gmail.com>
2022-12-20 10:52:58 +0800

    add readme for dynamic shape problem

---
+++ dynamic_shape/README.md
@@ -0,0 +1,10 @@
+# Dynamic Shape Problem
+
+## Issue: 
+
+https://github.com/NVIDIA/TensorRT/issues/2457
+
+## Solutions:
+
+1. Using multi-profile (Tensorrt V8.4)
+2. By setting preview feature FASTER_DYNAMIC_SHAPES_0805 (after Tensorrt V8.5.1)
---
+++ setup.sh
@@ -0,0 +1,10 @@
+# Tensorrt Lib
+export TRT_LIBPATH='/workspace/TensorRT-8.5.1.7/lib'
+export LD_LIBRARY_PATH="${TRT_LIBPATH}:/usr/local/cuda/lib64"
+
+# Tensorrt OSS
+export TRT_OSSPATH='/workspace/TensorRT'
+
+# Tensorrt Plugin
+# export PLUGIN_LIBS="$TRT_OSSPATH/build/out/libnvinfer_plugin.so"
+# export LD_PRELOAD=${PLUGIN_LIBS}
