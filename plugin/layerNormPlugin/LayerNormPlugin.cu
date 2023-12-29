8e238378e26653f86d00d035b4b8237d7710afae
col-in-coding <hxl.colin@gmail.com>
2023-12-29 13:32:56 +0800

    layernorm plugin add epsilon

--- plugin/layerNormPlugin/LayerNormPlugin.cu
+++ plugin/layerNormPlugin/LayerNormPlugin.cu
@@ -1,23 +1,114 @@
-#include <iostream>
-#include <chrono>
-#include <thread>
-
 #include "LayerNormPlugin.h"
 
 using namespace nvinfer1;
+using namespace plugin;
 
-PluginFieldCollection LayerNormPluginCreator::fc_{};
-std::vector<PluginField> LayerNormPluginCreator::attr_;
+using nvinfer1::plugin::LayerNormPlugin;
+using nvinfer1::plugin::LayerNormPluginCreator;
 
-int32_t LayerNormPlugin::enqueue(const PluginTensorDesc* inputDesc, const PluginTensorDesc* outputDesc,
+namespace
+{
+char const* kLAYER_NORM_PLUGIN_NAME{"MyLayerNorm"};
+char const* kLAYER_NORM_PLUGIN_VERSION{"1"};
+size_t constexpr kSERIALIZATION_SIZE{sizeof(float)};
+} // namespace
+
+LayerNormPlugin::LayerNormPlugin(std::string const& name, float mEpsilon)
+    : mName(name)
+    , mEpsilon(mEpsilon)
+{
+}
+
+LayerNormPlugin::LayerNormPlugin(std::string const& name, void const* buffer, size_t length)
+    : mName(name)
+{
+    PLUGIN_VALIDATE(buffer != nullptr);
+    PLUGIN_VALIDATE(length == kSERIALIZATION_SIZE);
+
+    char const* d = static_cast<char const*>(buffer);
+    char const* a = d;
+
+    mEpsilon = read<float>(d);
+
+    PLUGIN_VALIDATE(d == a + length);
+}
+
+IPluginV2DynamicExt* LayerNormPlugin::clone() const noexcept
+{
+    try
+    {
+        auto plugin = new LayerNormPlugin(*this);
+        plugin->setPluginNamespace(mNameSpace.c_str());
+        return plugin;
+    }
+    catch (std::exception const& e)
+    {
+        caughtError(e);
+    }
+    return nullptr;
+}
+
+int32_t LayerNormPlugin::getNbOutputs() const noexcept
+{
+    return 1;
+}
+
+DataType LayerNormPlugin::getOutputDataType(int32_t index, DataType const* inputTypes, int32_t nbInputs) const noexcept
+{
+    return inputTypes[0];
+}
+
+DimsExprs LayerNormPlugin::getOutputDimensions(
+    int32_t outputIndex, DimsExprs const* inputs, int32_t nbInputs, IExprBuilder& exprBuilder) noexcept
+{
+    return inputs[0];
+}
+
+bool LayerNormPlugin::supportsFormatCombination(
+    int32_t pos, PluginTensorDesc const* inOut, int32_t nbInputs, int32_t nbOutputs) noexcept
+{
+
+    switch (pos)
+    {
+    case 0:
+        return ((inOut[0].type == DataType::kFLOAT || inOut[0].type == DataType::kHALF)
+            && (inOut[0].format == TensorFormat::kLINEAR))
+            || ((inOut[0].type == DataType::kINT8)
+            && (inOut[0].format == TensorFormat::kCHW4 || inOut[0].format == TensorFormat::kCHW32));
+    case 1:
+    case 2:
+        return (inOut[pos].type == inOut[0].type)
+            || ((inOut[0].type == DataType::kINT8) && (inOut[pos].type == DataType::kHALF));
+    case 3: return (inOut[pos].type == inOut[0].type) && (inOut[pos].format == inOut[0].format);
+    default: // should NOT be here!
+        return false;
+    }
+    return false;
+}
+
+void LayerNormPlugin::configurePlugin(
+    DynamicPluginTensorDesc const* in, int32_t nbInputs, DynamicPluginTensorDesc const* out, int32_t nbOutputs) noexcept
+{
+}
+
+size_t LayerNormPlugin::getWorkspaceSize(
+    PluginTensorDesc const* inputs, int32_t nbInputs, PluginTensorDesc const* outputs, int32_t nbOutputs) const noexcept
+{
+    return 0;
+}
+
+int32_t LayerNormPlugin::enqueue(PluginTensorDesc const* inputDesc, PluginTensorDesc const* outputDesc,
     void const* const* inputs, void* const* outputs, void* workspace, cudaStream_t stream) noexcept
 {
-    WHERE_AM_I();
-    const int gridSize = inputDesc[0].dims.d[0] * inputDesc[0].dims.d[1];
-    const int nHiddenDimension = inputDesc[0].dims.d[2];
-    const float epsilon = 1e-6;
 
-    int status = -1;
+    int32_t gridSize = inputDesc[0].dims.d[0] * inputDesc[0].dims.d[1];
+    int32_t nHiddenSize = 1;
+    for (int32_t i = 2; i < inputDesc[0].dims.nbDims; ++i)
+    {
+        nHiddenSize *= inputDesc[0].dims.d[i];
+    }
+    int32_t status = -1;
+
     switch (inputDesc[0].type)
     {
     case DataType::kFLOAT:
@@ -28,28 +119,161 @@ int32_t LayerNormPlugin::enqueue(const PluginTensorDesc* inputDesc, const Plugin
         auto const input = static_cast<float const*>(inputs[0]);
         auto const gamma = static_cast<float const*>(inputs[1]);
         auto const beta = static_cast<float const*>(inputs[2]);
-        auto output = static_cast<float *>(outputs[0]);
-        status = computeLayerNorm<float>(gridSize, nHiddenDimension, input, output, gamma, beta, epsilon, stream);
+        auto output = static_cast<float*>(outputs[0]);
+
+        status = computeLayerNorm<float>(gridSize, nHiddenSize, input, output, gamma, beta, mEpsilon, stream);
         break;
     }
     case DataType::kHALF:
     {
-        // printf("===> using FP16 kernel\n");
+
         auto const input = static_cast<half const*>(inputs[0]);
         auto const gamma = static_cast<half const*>(inputs[1]);
         auto const beta = static_cast<half const*>(inputs[2]);
-        auto output = static_cast<half *>(outputs[0]);
-        status = computeLayerNorm<half>(gridSize, nHiddenDimension, input, output, gamma, beta, epsilon, stream);
+        auto output = static_cast<half*>(outputs[0]);
+
+        status = computeLayerNorm<half>(gridSize, nHiddenSize, input, output, gamma, beta, mEpsilon, stream);
         break;
     }
+    // case DataType::kINT8:
+    // {
+    //     float const dqScaleIn = inputDesc[0].scale;
+    //     float const qScale = 1.f / outputDesc[0].scale;
+    //     auto const input = static_cast<int8_t const*>(inputs[0]);
+    //     auto output = static_cast<int8_t*>(outputs[0]);
+    //     auto const gamma = static_cast<half const*>(inputs[1]);
+    //     auto const beta = static_cast<half const*>(inputs[2]);
+
+    //     status = computeLayerNormQDQ(
+    //         gridSize, nHiddenSize, input, gamma, beta, output, dqScaleIn, qScale, mEpsilon, stream);
+    //     break;
+    // }
     default:
     {
-        printf("Datatype not implemented yet. %s, %d", __FILE__, __LINE__);
+        PLUGIN_FAIL("DataType not implemented yet");
         break;
     }
     }
     return status;
 }
+
+void LayerNormPlugin::destroy() noexcept
+{
+    delete this;
+}
+
+int32_t LayerNormPlugin::initialize() noexcept
+{
+    return 0;
+}
+
+void LayerNormPlugin::terminate() noexcept {}
+
+size_t LayerNormPlugin::getSerializationSize() const noexcept
+{
+    return kSERIALIZATION_SIZE;
+}
+
+void LayerNormPlugin::serialize(void* buffer) const noexcept
+{
+    PLUGIN_ASSERT(buffer != nullptr);
+    char* d = static_cast<char*>(buffer);
+    char* a = d;
+    write(d, mEpsilon); // float
+    PLUGIN_ASSERT(d == a + getSerializationSize());
+}
+
+void LayerNormPlugin::setPluginNamespace(char const* pluginNamespace) noexcept
+{
+    mNameSpace = pluginNamespace;
+}
+
+char const* LayerNormPlugin::getPluginNamespace() const noexcept
+{
+    return mNameSpace.c_str();
+}
+
+char const* LayerNormPlugin::getPluginType() const noexcept
+{
+    return kLAYER_NORM_PLUGIN_NAME;
+}
+
+char const* LayerNormPlugin::getPluginVersion() const noexcept
+{
+    return kLAYER_NORM_PLUGIN_VERSION;
+}
+
+PluginFieldCollection LayerNormPluginCreator::mFC{};
+std::vector<PluginField> LayerNormPluginCreator::mPluginAttributes;
+
+LayerNormPluginCreator::LayerNormPluginCreator()
+{
+    mPluginAttributes.clear();
+    mPluginAttributes.emplace_back(PluginField("epsilon", nullptr, PluginFieldType::kFLOAT32, 1));
+    mFC.nbFields = mPluginAttributes.size();
+    mFC.fields = mPluginAttributes.data();
+}
+
+LayerNormPluginCreator::~LayerNormPluginCreator() {}
+
+IPluginV2* LayerNormPluginCreator::createPlugin(char const* name, PluginFieldCollection const* fc) noexcept
+{
+    try
+    {
+        PLUGIN_VALIDATE(fc != nullptr);
+        PluginField const* fields = fc->fields;
+
+        // default values
+        float mEpsilon = 1e-5F;
+
+        for (int32_t i = 0; i < fc->nbFields; ++i)
+        {
+            char const* attrName = fields[i].name;
+            if (!strcmp(attrName, "epsilon"))
+            {
+                PLUGIN_VALIDATE(fields[i].type == PluginFieldType::kFLOAT32);
+                mEpsilon = static_cast<float>(*(static_cast<float const*>(fields[i].data)));
+            }
+        }
+        return new LayerNormPlugin(name, mEpsilon);
+    }
+    catch (std::exception const& e)
+    {
+        caughtError(e);
+    }
+    return nullptr;
+}
+
+IPluginV2* LayerNormPluginCreator::deserializePlugin(
+    char const* name, void const* serialData, size_t serialLength) noexcept
+{
+    try
+    {
+        PLUGIN_VALIDATE(serialData != nullptr);
+        return new LayerNormPlugin(name, serialData, serialLength);
+    }
+    catch (std::exception const& e)
+    {
+        caughtError(e);
+    }
+    return nullptr;
+}
+
+char const* LayerNormPluginCreator::getPluginName() const noexcept
+{
+    return kLAYER_NORM_PLUGIN_NAME;
+}
+
+char const* LayerNormPluginCreator::getPluginVersion() const noexcept
+{
+    return kLAYER_NORM_PLUGIN_VERSION;
+}
+
+PluginFieldCollection const* LayerNormPluginCreator::getFieldNames() noexcept
+{
+    return &mFC;
+}
+
 
 REGISTER_TENSORRT_PLUGIN(LayerNormPluginCreator);
 
--- test/testLayerNormPlugin.py
+++ test/testLayerNormPlugin.py
@@ -16,16 +16,15 @@
 
 import os
 import ctypes
+import torch
 import numpy as np
 from cuda import cudart  # 使用 cuda runtime API
 import tensorrt as trt
 # import nvtx
 
 soFilePath = '/workspace/Github/Tensorrt-CV/build/out/liblayernorm_plugin.so'
-epsilon = 6e-6
-
-
-np.random.seed(97)
+epsilon = 1e-6
+np.random.seed(31193)
 
 
 def check(a, b, weak=False, checkEpsilon=1e-5):
@@ -40,37 +39,45 @@ def check(a, b, weak=False, checkEpsilon=1e-5):
     print("check:%s, absDiff=%f, relDiff=%f" % (res, diff0, diff1))
 
 
-def layerNormCPU(bufferH):
+def layerNormCPU(bufferH, epsilon, torch_mode=True, isFP16=False):
     _x, gamma, beta = bufferH
-    nHiddenSize = bufferH[0].shape[2]
-    _0 = np.mean(_x, 2)[:, :, np.newaxis]
-    _1 = _x - _0
-    _2 = _1 * _1
-    _3 = np.mean(_2, 2)[:, :, np.newaxis]
-    _4 = np.array(epsilon, dtype=np.float32)
-    _5 = _4.reshape(1, 1, 1)
-    _6 = _3 + _5
-    _7 = np.sqrt(_6)
-    _8 = 1 / _7  # 1/sqrt(...)
-    _9 = gamma
-    _10 = _9.reshape(1, 1, nHiddenSize)
-    _11 = _8 * _10  # gamma/sqrt(...)
-    _12 = _0 * _11  # bμ/sqrt(...)
-    _13 = beta
-    _14 = _13.reshape(1, 1, nHiddenSize)
-    _15 = _14 - _12  # beta-bμ/sqrt(...)
-    _16 = _x * _11  # bx/sqrt(...)
-    _17 = _15 + _16  # gamma(x-μ)/sqrt(...)+beta
-    _18 = _17.reshape(bufferH[0].shape[0], bufferH[0].shape[1], bufferH[0].shape[2])
-    return _18
+    if not isFP16:
+        _x = _x.astype(np.float32)
+        gamma = gamma.astype(np.float32)
+        beta = beta.astype(np.float32)
+    if torch_mode:
+        _0 = torch.nn.functional.layer_norm(torch.from_numpy(_x), normalized_shape=_x.shape[-1:], weight=torch.from_numpy(gamma), bias=torch.from_numpy(beta), eps=epsilon)
+        return [_0.numpy()]
+    else:
+        nHiddenSize = bufferH[0].shape[2]
+        _0 = np.mean(_x, 2)[:, :, np.newaxis]
+        _1 = _x - _0
+        _2 = _1 * _1
+        _3 = np.mean(_2, 2)[:, :, np.newaxis]
+        _4 = np.array(epsilon, dtype=np.float32)
+        _5 = _4.reshape(1, 1, 1)
+        _6 = _3 + _5
+        _7 = np.sqrt(_6)
+        _8 = 1 / _7  # 1/sqrt(...)
+        _9 = gamma
+        _10 = _9.reshape(1, 1, nHiddenSize)
+        _11 = _8 * _10  # gamma/sqrt(...)
+        _12 = _0 * _11  # bμ/sqrt(...)
+        _13 = beta
+        _14 = _13.reshape(1, 1, nHiddenSize)
+        _15 = _14 - _12  # beta-bμ/sqrt(...)
+        _16 = _x * _11  # bx/sqrt(...)
+        _17 = _15 + _16  # gamma(x-μ)/sqrt(...)+beta
+        _18 = _17.reshape(bufferH[0].shape[0], bufferH[0].shape[1], bufferH[0].shape[2])
+        return [_18]
 
 
 def getLayerNormPlugin():
     for c in trt.get_plugin_registry().plugin_creator_list:
         # print(c.name)
-        # if c.name == 'MyLayerNorm':
-        if c.name == 'LayerNorm':
-            return c.create_plugin(c.name, trt.PluginFieldCollection([]))
+        if c.name == 'MyLayerNorm':
+        # if c.name == 'LayerNorm':
+            return c.create_plugin(c.name, trt.PluginFieldCollection([trt.PluginField("epsilon", np.float32(epsilon), trt.PluginFieldType.FLOAT32)]))
     return None
 
 
@@ -82,33 +89,23 @@ def run(input_shape, isFP16=False):
     ctypes.cdll.LoadLibrary(soFilePath)
 
     builder = trt.Builder(logger)
-    network = builder.create_network(1 << 0)
+    network = builder.create_network(1 << int(trt.NetworkDefinitionCreationFlag.EXPLICIT_BATCH))
     config = builder.create_builder_config()
-    config.set_memory_pool_limit(trt.MemoryPoolType.WORKSPACE, 6 << 30)
+    # config.set_memory_pool_limit(trt.MemoryPoolType.WORKSPACE, 6 << 30)
 
-    inputTensorList = []
     if isFP16:
         config.set_flag(trt.BuilderFlag.FP16)
-        config.set_flag(trt.BuilderFlag.STRICT_TYPES)
-        inputTensorList.append(
-            network.add_input('inputT', trt.float16, [-1, -1, nEmbedding]))
-        inputTensorList.append(
-            network.add_input('gemmaT', trt.float16, [1, 1, nEmbedding]))
-        inputTensorList.append(
-            network.add_input('betaT', trt.float16, [1, 1, nEmbedding]))
-    else:
-        inputTensorList.append(
-            network.add_input('inputT', trt.float32, [-1, -1, nEmbedding]))
-        inputTensorList.append(
-            network.add_input('gemmaT', trt.float32, [1, 1, nEmbedding]))
-        inputTensorList.append(
-            network.add_input('betaT', trt.float32, [1, 1, nEmbedding]))
+        # config.set_flag(trt.BuilderFlag.STRICT_TYPES)
+
+    inputT0 = network.add_input('inputT', trt.float16 if isFP16 else trt.float32, [-1, -1, nEmbedding])
+    inputT1 = network.add_input('gemmaT', trt.float16 if isFP16 else trt.float32, [nEmbedding])
+    inputT2 = network.add_input('betaT', trt.float16 if isFP16 else trt.float32, [nEmbedding])
 
     profile = builder.create_optimization_profile()
-    profile.set_shape('inputT', [1, 1, nEmbedding], [nBS, nSL, nEmbedding], [nBS, nSL, nEmbedding])
     config.add_optimization_profile(profile)
+    profile.set_shape(inputT0.name, [1, 1, nEmbedding], [nBS, nSL, nEmbedding], [nBS, nSL, nEmbedding])
 
-    pluginLayer = network.add_plugin_v2(inputTensorList, getLayerNormPlugin())
+    pluginLayer = network.add_plugin_v2([inputT0, inputT1, inputT2], getLayerNormPlugin())
     # force trt to use fp16 mode
     # pluginLayer.precision = trt.DataType.HALF
 
@@ -123,14 +120,12 @@ def run(input_shape, isFP16=False):
     engine = trt.Runtime(logger).deserialize_cuda_engine(engineString)
 
     context = engine.create_execution_context()
-    context.set_binding_shape(0, [nBS, nSL, nEmbedding])
+    context.set_binding_shape(0, input_shape)
+    # context.set_input_shape(engine.get_tensor_name(0), [nBS, nSL, nEmbedding])
     print("Binding all? %s" %
           (["No", "Yes"][int(context.all_binding_shapes_specified)]))
 
-    nInput = [
-        engine.get_tensor_mode(engine.get_tensor_name(i))
-        for i in range(engine.num_bindings)
-    ].count(trt.TensorIOMode.INPUT)
+    nInput = np.sum([engine.binding_is_input(i) for i in range(engine.num_bindings)])
     nOutput = engine.num_bindings - nInput
     # for i in range(engine.num_bindings):
     #     print(
@@ -139,25 +134,21 @@ def run(input_shape, isFP16=False):
     #         context.get_binding_shape(i))
 
     bufferH = []
-    shape = nBS, nSL, nEmbedding
-    if isFP16:
-        bufferH.append(
-            np.random.rand(np.prod(shape)).reshape(shape).astype(np.float16) * 2 - 1)
-        # gamma
-        bufferH.append(np.random.rand(1, 1, nEmbedding).astype(np.float16))
-        # beta
-        bufferH.append(np.random.rand(1, 1, nEmbedding).astype(np.float16))
-    else:
-        bufferH.append(
-            np.random.rand(np.prod(shape)).reshape(shape).astype(np.float32) * 2 - 1)
-        # gamma
-        bufferH.append(np.random.rand(1, 1, nEmbedding).astype(np.float32))
-        # beta
-        bufferH.append(np.random.rand(1, 1, nEmbedding).astype(np.float32))
 
-    bufferH.append(
-        np.empty(context.get_binding_shape(3),
-                 dtype=trt.nptype(engine.get_binding_dtype(3))))
+    bufferH.append(np.random.rand(np.prod(input_shape)).reshape(input_shape).astype(np.float16 if isFP16 else np.float32) * 2 - 1)
+    # gamma
+    bufferH.append(np.random.rand(nEmbedding).astype(np.float16 if isFP16 else np.float32))
+    # beta
+    bufferH.append(np.random.rand(nEmbedding).astype(np.float16 if isFP16 else np.float32))
+    # # gamma
+    # bufferH.append(np.ones(nEmbedding).astype(np.float16 if isFP16 else np.float32))
+    # # beta
+    # bufferH.append(np.zeros(nEmbedding).astype(np.float16 if isFP16 else np.float32))
+
+    for i in range(nOutput):
+        bufferH.append(
+            np.empty(context.get_binding_shape(nInput + i),
+                    dtype=trt.nptype(engine.get_binding_dtype(nInput + i))))
     # print("===> output type :", engine.get_binding_dtype(3))
     # exit(0)
 
@@ -166,7 +157,7 @@ def run(input_shape, isFP16=False):
         bufferD.append(cudart.cudaMalloc(bufferH[i].nbytes)[1])
 
     for i in range(nInput):
-        cudart.cudaMemcpy(bufferD[i], bufferH[i].ctypes.data,
+        cudart.cudaMemcpy(bufferD[i], np.ascontiguousarray(bufferH[i].reshape(-1)).ctypes.data,
                           bufferH[i].nbytes,
                           cudart.cudaMemcpyKind.cudaMemcpyHostToDevice)
 
@@ -178,26 +169,24 @@ def run(input_shape, isFP16=False):
                           bufferH[i].nbytes,
                           cudart.cudaMemcpyKind.cudaMemcpyDeviceToHost)
 
-
-    temp1 = bufferH[-1]
-    temp2 = layerNormCPU(bufferH[:3])
-    # print(temp1.sum(), temp2.sum())
+    temp1 = bufferH[nInput:]
+    temp2 = layerNormCPU(bufferH[:3], epsilon)
+    print(temp1[0].sum(), temp2[0].sum())
     # print(bufferH[0])
     # print(temp1)
     # print(temp2)
 
     # check(temp1, temp2, weak=False)
-    check(temp1, temp2, weak=True)
+    check(temp1[0], temp2[0], weak=True)
 
     for b in bufferD:
         cudart.cudaFree(b)
 
 
 if __name__ == '__main__':
-    os.system("rm -f ./*.trt")
-    np.set_printoptions(precision=4, linewidth=200, suppress=True)
+    np.set_printoptions(precision=3, linewidth=200, suppress=True)
 
     # run((4, 64, 32), isFP16=True)
     # run((4, 64, 256), isFP16=True)
-    run((4, 64, 1024), isFP16=False)
-    # run((4, 64, 768), isFP16=True)
+    run((16, 64, 1024), isFP16=False)
+    # run((4, 64, 4096), isFP16=False)
